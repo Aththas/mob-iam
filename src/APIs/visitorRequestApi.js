f5152c7ec309a119e3746fb558ce63e60d8922ee
aththas <aththasrizwan123@gmail.com>
2024-12-08 23:13:44 +0530

    completed the get pending visitor requests with search option

--- src/APIs/visitorRequestApi.js
+++ src/APIs/visitorRequestApi.js
@@ -1,16 +1,29 @@
 import axiosInstance from './axiosInstance'
 
-export const getMyVisitorRequests = (page, size, sortBy, ascending) => 
-    axiosInstance.get('/visitor/viewVisitorEntryRequestByUser', {params:  {page, size, sortBy, ascending}});
-
 export const addVisitorRequest = (data) =>
     axiosInstance.post('/visitor/addVisitorRequest', data);
 
+
+export const getMyVisitorRequests = (page, size, sortBy, ascending) => 
+    axiosInstance.get('/visitor/viewVisitorEntryRequestByUser', {params: {page, size, sortBy, ascending}});
+
 export const searchMyVisitorRequestsByKeyword = (page, size, sortBy, ascending, keyword) => 
-    axiosInstance.get('/visitor/searchVisitorEntryRequestByUser', {params:  {page, size, sortBy, ascending, keyword}});
+    axiosInstance.get('/visitor/searchVisitorEntryRequestByUser', {params: {page, size, sortBy, ascending, keyword}});
+
 
 export const getApprovedVisitorRequests = (page, size, sortBy, ascending) => 
-    axiosInstance.get('/visitor/viewAcceptVisitorEntryRequest', {params:  {page, size, sortBy, ascending}});
+    axiosInstance.get('/visitor/viewAcceptVisitorEntryRequest', {params: {page, size, sortBy, ascending}});
 
 export const searchApprovedVisitorRequests = (page, size, sortBy, ascending, keyword) => 
-    axiosInstance.get('/visitor/searchAcceptVisitorEntryRequest', {params:  {page, size, sortBy, ascending, keyword}});
+    axiosInstance.get('/visitor/searchAcceptVisitorEntryRequest', {params: {page, size, sortBy, ascending, keyword}});
+
+
+export const getPendingVisitorRequests = (page, size, sortBy, ascending) =>
+    axiosInstance.get('/visitor/viewPendingVisitorEntryRequest', {params: {page, size, sortBy, ascending}});
+
+export const searchPendingVisitorRequests = (page, size, sortBy, ascending, keyword) => 
+    axiosInstance.get('/visitor/searchPendingVisitorEntryRequest', {params: {page, size, sortBy, ascending, keyword}});
+
+
+export const getNotPendingVisitorRequests = (page, size, sortBy, ascending) =>
+    axiosInstance.get('/visitor/viewNotPendingVisitorEntryRequest', {params: {page, size, sortBy, ascending}});
--- src/Components/Security-Approval-Section/SecurityApprovalVisitor.jsx
+++ src/Components/Security-Approval-Section/SecurityApprovalVisitor.jsx
@@ -1,56 +1,55 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './SecurityApproval.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faHistory, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
 import MainHeading from '../Main-Heading/MainHeading';
+import toastr from '../toastr-config/ToastrConfig';
+import { getNotPendingVisitorRequests, getPendingVisitorRequests, searchPendingVisitorRequests } from '../../APIs/visitorRequestApi';
+import Loading from '../Loading-Spinner/Loading';
 
 const SecurityApprovalVisitor = () => {
-    const [isHistory, setHistory] = useState(true);
 
-    const userDetails = [
-        {
-            id: 1,
-            first_name: 'Aththas',
-            last_name: 'Rizwan',
-            nic: '990623686V',
-            department: 'Information Systems',
-            start: '2024-03-25',
-            end: '2024-12-25',
-            approval: 'accept',
-            permission: 'accept',
-            status: 'active',
-            requester: 'Amaya',
-            approver: 'Musthalie'
-        },
-        {
-            id: 2,
-            first_name: 'Deelaka',
-            last_name: 'Gamage',
-            nic: '990622226V',
-            department: 'Information Systems',
-            start: '2024-03-25',
-            end: '2024-11-01',
-            approval: 'pending',
-            permission: 'pending',
-            status: 'active',
-            requester: 'Amaya',
-            approver: 'Musthalie'
-        },
-        {
-            id: 3,
-            first_name: 'Amar',
-            last_name: 'Shenan',
-            nic: '990623386V',
-            department: 'Information Systems',
-            start: '2024-03-25',
-            end: '2024-11-15',
-            approval: 'reject',
-            permission: 'reject',
-            status: 'active',
-            requester: 'Amaya',
-            approver: 'Musthalie'
+    const [isHistory, setHistory] = useState(false);//true means history, it means alreay reacted ones, so notPending ones...
+    const sortBy = 'id'; const ascending = true;
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [totalPages, setTotalPages] = useState(1);
+    const [visitorRequestList, setVisitorRequestList] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState('');
+    const [searchOn, setSearchOn] = useState(false);
+    const [searchCount, setSearchCount] = useState(0);
+
+    const fetchVisitorRequestDetails = useCallback ( async () => {
+        setLoading(true);
+        try{
+            let response;
+            if(isHistory){
+                response = await getNotPendingVisitorRequests(page, size, sortBy, ascending);
+            }else{
+                response = searchOn
+                    ? await searchPendingVisitorRequests(page, size, sortBy, ascending, search)
+                    : await getPendingVisitorRequests(page, size, sortBy, ascending);
+            }
+
+            if(response.data.success){
+                setVisitorRequestList(response.data.data);
+                setTotalPages(Math.ceil(response.data.message/size));
+            }else{
+                setVisitorRequestList([]);
+                setTotalPages(1);
+            }
+        }catch(error){
+            console.log(error);
+            toastr.error(error.response?.data?.message || "Network Error");
+        }finally{
+            setLoading(false);
         }
-    ];
+    }, [page, size, sortBy, ascending, isHistory, searchOn, searchCount]);
+
+    useEffect(() => {
+        fetchVisitorRequestDetails();
+    }, [fetchVisitorRequestDetails]);
 
     const getColor = (status) =>{
         if(status === 'accept') return "green";
@@ -59,71 +58,116 @@ const SecurityApprovalVisitor = () => {
         return "black";
     }
 
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+    };
+
+    const handleSizeChange = (e) => {
+        setSize(Number(e.target.value));
+        setPage(0);
+    };
+
+    const filterChange = () => {
+        setHistory((cur) => !cur);
+        setPage(0);
+    };
+
+    const handleSearch = () => {
+        if(search === '' || search === null){
+            setSearchOn(false);
+        }else{
+            setSearchOn(true);
+            setSearchCount((count) => count+1);
+        }
+        setPage(0);
+    }; 
+
   return (
     <div className='main-user'>
-        <MainHeading icon={isHistory ? faPen : faHistory} heading={`Visitors Entry Approval Request ${!isHistory ? 'History' :''}`}/>
+        {loading && <Loading/>}
+        <MainHeading icon={isHistory ? faHistory : faPen} heading={`Visitors Entry Approval Request ${isHistory ? 'History' :''}`}/>
         <div className="main-section-container">
             <div className="main-content">
                 <div className="page-filter-and-add-new">
                     <div className="page-filter">
                         <label>
                             Entries per page: 
-                            <select>
-                                <option value={8}>8</option>
+                            <select value={size} onChange={handleSizeChange}>
                                 <option value={10}>10</option>
                                 <option value={25}>25</option>
                                 <option value={50}>50</option>
+                                <option value={75}>75</option>
                                 <option value={100}>100</option>
                             </select>
                         </label>
                     </div>
                     <div className="search">
-                        <input className='search-input' type='text' placeholder='search...' id='search' name='search' />
-                        <button className='search-btn'>Search</button>
+                        <input className='search-input' type='text' placeholder='search...' id='search' name='search' 
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                        <button className='search-btn' onClick={handleSearch}>Search</button>
                     </div>
-                    <button className='filter-btn' onClick={() => setHistory((cur) => !cur)}>
-                        <FontAwesomeIcon icon={isHistory ? faHistory : faEnvelopeOpenText} className='icon' />
+                    <button className='filter-btn' onClick={filterChange}>
+                        <FontAwesomeIcon icon={isHistory ? faEnvelopeOpenText : faHistory} className='icon' />
                     </button>
                 </div>
                 <div className="user-table">
                     <table className="styled-table">
                         <thead>
                             <tr>
-                                <th>NIC</th>
+                                <th>#</th>
+                                <th>NIC/VID</th>
                                 <th>Name</th>
+                                <th>Company</th>
                                 <th>Allowed Department</th>
                                 <th>Start Date</th>
                                 <th>End Date</th>
+                                <th>Night Stay</th>
                                 <th>Requester</th>
-                                <th>{isHistory ? 'Action':'My Approval'}</th>
+                                <th>{isHistory ? 'My Approval':'Action'}</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {userDetails.map((user,index) => (
+                            {visitorRequestList.length > 0 ? (
+                                visitorRequestList.map((visitorRequest,index) => (
                                 <tr key={index}>
-                                    <td>{user.nic}</td>
-                                    <td>{`${user.first_name} ${user.last_name}`}</td>
-                                    <td>{user.department}</td>
-                                    <td>{user.start}</td>
-                                    <td>{user.end}</td>
-                                    <td>{user.requester}</td>
+                                    <td>{index + 1 + page * size}</td>
+                                    <td>{visitorRequest.visitor.verificationId}</td>
+                                    <td>{visitorRequest.visitor.name}</td>
+                                    <td>{visitorRequest.visitor.company}</td>
+                                    <td>{visitorRequest.department}</td>
+                                    <td>{visitorRequest.startDate}</td>
+                                    <td>{visitorRequest.endDate}</td>
+                                    <td>{visitorRequest.nightStay.toUpperCase()}</td>
+                                    <td>
+                                        {visitorRequest.user.firstName} {visitorRequest.user.lastName} <br/>
+                                        {visitorRequest.user.designation}<br/>
+                                        {visitorRequest.user.contact}
+                                    </td>
                                     {isHistory ? (
-                                        <td>
-                                            <button className="accept-btn">accept</button>
-                                            <button className="reject-btn">reject</button>
+                                        <td style={{color:getColor(visitorRequest.permission), fontWeight:'900'}}>
+                                            {visitorRequest.permission.toUpperCase()}
                                         </td>
                                     ) : (
-                                        <td style={{color:getColor(user.approval), fontWeight:'900'}}>{user.approval}</td>
+                                        <td>
+                                            <button className="accept-btn">ACCEPT</button>
+                                            <button className="reject-btn">REJECT</button>
+                                        </td>
                                     )} 
                                 </tr>
-                            ))}
+                            ))):(
+                                <tr>
+                                    <td colSpan="10" style={{textAlign:'center', color:'darkgray'}}>No Details Found</td>
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
                 <div className="pagination">
-                    <button className='prev-btn'>Previous</button>
-                    <p>Page 1 of 1</p>
-                    <button className='next-btn'>Next</button>
+                    <button className='prev-btn' onClick={() => handlePageChange(page-1)} disabled={page < 1}>Previous</button>
+                    <p>Page {page+1} of {totalPages}</p>
+                    <button className='next-btn' onClick={() => handlePageChange(page+1)} disabled={page+1 === totalPages}>Next</button>
                 </div>
             </div>
         </div>
